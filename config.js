15eaca3e0c87a712375cbcc96bb5644b4c406af8
knolegsb <seon2you@hotmail.com>
2016-01-13 20:37:19 -0800

    login api

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database" : "",
+	"database" : "mongodb://root:abc123@ds045475.mongolab.com:45475/userstory",
 	"port" : process.env.PORT || 3000,
 	"secretKey" : "YourSecretKey"
 }
